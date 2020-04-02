925e8e46ac0d70e2bb5896ed11f723305730c827
Aidan <redyelruc1@hotmail.com>
2020-04-02 18:08:49 +0100

    making a sandwich menu with prices - using input validation

--- SandwichShopMenuChoices.py
+++ SandwichShopMenuChoices.py
@@ -1,24 +1,43 @@
 import pyinputplus as pyip
 
-types_of_food = {'bread': ['wheat', 'white', 'sourdough'], 'protein type':
+types_of_food = {'bread': ['wheat', 'white', 'sourdough'], 'filling':
     ['chicken', 'turkey', 'ham', 'tofu']}
-print('Welcome to Fatso\'s Sanwiches')
+
+prices = {'wheat' : 1.5, 'white': 1.6, 'sourdough': 2, 'chicken': 1, 'turkey':1.2, 'ham': 1.5,
+          'tofu': 2, 'mozzarella': 0.5, 'cheddar': 0.4,'parmesan': 0.5, 'ketchup': 0.2,'brown sauce':0.2,
+          'mayo': 0.2, 'salad cream': 0.1}
+
+# function to add choie made to list and to fetch its price
+def process_choice(chosen):
+    choice_made.append(chosen)
+    return prices[chosen]
+
+print('Welcome to Fatso\'s Sandwiches')
+
 while True:
     choice_made =[]
+    sandwich_price = 0
     for food, choices in types_of_food.items():
         print('What type of %s would you like?'% food )
-        choice_made.append(pyip.inputMenu(types_of_food[food], lettered=True))
+        item_chosen = pyip.inputMenu(choices, lettered=True)
+        sandwich_price = sandwich_price + process_choice(item_chosen)
+    # check if wants cheese and find out which one
     if pyip.inputYesNo('Would you like cheese?') == 'yes':
         print('What type of cheese would you like?')
-        choice_made.append(pyip.inputMenu(['mozzarella', 'cheddar', 'parmesan'], lettered = True))
+        item_chosen = pyip.inputMenu(['mozzarella', 'cheddar', 'parmesan'], lettered = True)
+        sandwich_price = sandwich_price + process_choice(item_chosen)
     else:
         choice_made.append('')
+    # check if wants sauce and find out which one
     if pyip.inputYesNo('Would you like any sauce?') == 'yes':
         print('What type of sauce would you like?')
-        choice_made.append(pyip.inputMenu(['ketchup', 'brown sauce', 'mayo', 'salad cream'], lettered = True))
+        item_chosen = pyip.inputMenu(['ketchup', 'brown sauce', 'mayo', 'salad cream'], lettered = True)
+        sandwich_price = sandwich_price + process_choice(item_chosen)
     else:
         choice_made.append('')
+    # shows order so far
     print('So that\'s a sandwich on %s bread with %s,  %s, %s.'
           %(choice_made[0], choice_made[1], choice_made[2], choice_made[3]))
+    print('Your sandwich costs £%s'% sandwich_price)
     if pyip.inputYesNo('Would you like another sandwich?') == 'no':
         break
