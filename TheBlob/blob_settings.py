b0a5e321cc043eb38b758ede1d8d7f014119b43b
Aidan <redyelruc1@hotmail.com>
2020-04-11 19:56:54 +0100

    first two games

---
+++ TheBlob/blob_settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    '''a class of settings for my own mini game - the blob'''
+    def __init__(self):
+        '''initialise the game's settings'''
+        self.screen_width = 1000
+        self.screen_height = 550
+        self.screen_colour = (230,230,230)
